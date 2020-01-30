63ff39bf9522a2a02317ff96808cc997eb7459b8
Daniel Sun <danielsun12345@gmail.com>
2020-01-30 17:02:27 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ contacts = {
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
-        contacts_details_hash.each do |attribute, data|
+        contact_details_hash.each do |attribute, data|
           if attribute == :favorite_ice_cream_flavors
             data.delete_if {|ice_cream| ice_cream == "strawberry"}
           end 
